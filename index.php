821e15181a0a55ffff876dd01f45d04a2a329e4a
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-05-10 16:01:10 +0200

    inizio inserimento query

--- index.php
+++ index.php
@@ -2,14 +2,21 @@
   include_once __DIR__.'/./_partials/template/header.php';
 ?>
 
+
+
+
+
 <?php
+  // DATI X LA CONNESSIONE
   $servername = "localhost";
   $username = "root";
   $password = "root";
   $dbname = "db_hotel";
 
+  // METTO  IN UNA VARIABILE IL RISULTATO DELLA CONNESSIONE
   $conn = new mysqli($servername, $username, $password, $dbname);
 
+  // CHIEDO COM'E' ANDATA LA CONNESSIONE
   if ($conn && $conn->connect_error) {
     echo "Connection failed: " . $conn->connect_error;
   }
